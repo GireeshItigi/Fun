6b3b2c4a127128390d514a9af4ed69feac12426a
ssd <ssd@DESKTOP-8AP03RJ>
2023-10-05 14:07:10 +0530

    commit

--- Fun/Controllers/JokesController.cs
+++ Fun/Controllers/JokesController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Fun.Data;
 using Fun.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Fun.Controllers
 {
@@ -25,6 +26,18 @@ namespace Fun.Controllers
               return _context.Joke != null ? 
                           View(await _context.Joke.ToListAsync()) :
                           Problem("Entity set 'ApplicationDbContext.Joke'  is null.");
+        }
+
+        // GET: Jokes/ShowSearchForm 
+        public async Task<IActionResult> ShowSearchForm()
+        {
+            return View();
+        }
+
+        // Post: Jokes/ShowSearchResults 
+        public async Task<IActionResult> ShowSearchResults(String SearchPhrase)
+        {
+            return View("Index", await _context.Joke.Where(j => j.JokeQuestion.Contains(SearchPhrase)).ToListAsync());
         }
 
         // GET: Jokes/Details/5
@@ -46,6 +59,8 @@ namespace Fun.Controllers
         }
 
         // GET: Jokes/Create
+        [Authorize]
+        
         public IActionResult Create()
         {
             return View();
@@ -68,6 +83,9 @@ namespace Fun.Controllers
         }
 
         // GET: Jokes/Edit/5
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null || _context.Joke == null)
@@ -86,6 +104,7 @@ namespace Fun.Controllers
         // POST: Jokes/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,JokeQuestion,JokeAnswer")] Joke joke)
@@ -137,6 +156,7 @@ namespace Fun.Controllers
         }
 
         // POST: Jokes/Delete/5
+        [Authorize]
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
